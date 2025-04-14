d8d4b37cf3233348374409da0e81dfb32419d2de
matos.leticia0208 <matos.leticia0208@gmail.com>
2025-04-14 22:44:07 +0000

    mudança da função handleSubmit para o CardInicial; alteração da duração do hover dos botões

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,9 @@ export function App() {
   const [notaAvaliacao, setNotaAvaliacao] = useState(0)
   const [submited, setSubmited] = useState(false)
 
- 
-
-  function handleSubmit() {
-    if (notaAvaliacao !== 0) {
-      setSubmited(true)
-      return
-    }
-    alert("Please, choose a note!")
-  }
-
   return (
     submited === false? (
-     <CardInicial setNotaAvaliacao={setNotaAvaliacao} handleSubmit={handleSubmit}/>
+     <CardInicial setNotaAvaliacao={setNotaAvaliacao} setSubmited={setSubmited} notaAvaliacao={notaAvaliacao}/>
   ) : (
     <CardFinal notaAvaliacao={notaAvaliacao}/>
   )
--- src/components/CardPrincipal.jsx
+++ src/components/CardPrincipal.jsx
@@ -1,7 +1,14 @@
 import iconStar from "../assets/icon-star.svg"
 import { InputButton } from "./InputButton"
 
-export function CardInicial({setNotaAvaliacao, handleSubmit}) {
+export function CardInicial({setNotaAvaliacao, notaAvaliacao, setSubmited}) {
+    function handleSubmit() {
+        if (notaAvaliacao !== 0) {
+          setSubmited(true)
+          return
+        }
+        alert("Please, choose a note!")
+      }
 
     return(
         <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass max-w-103">
@@ -21,7 +28,7 @@ export function CardInicial({setNotaAvaliacao, handleSubmit}) {
                 <InputButton setNotaAvaliacao={setNotaAvaliacao} value={5}/>
             </div>
   
-            <button onClick={handleSubmit} className="bg-orange w-full uppercase tracking-1 font-bold rounded-3xl text-sm py-3 hover:bg-white hover:text-orange hover:cursor-pointer">Submit</button>
+            <button onClick={handleSubmit} className="bg-orange w-full uppercase tracking-1 font-bold rounded-3xl text-sm py-3 hover:bg-white hover:text-orange hover:cursor-pointer duration-500">Submit</button>
       </div>
     )
 }
